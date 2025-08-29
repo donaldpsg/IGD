e6e2546755ee4ef6908f3ff636ce5e130ad19037
unknown <donald@peaksolutionsid.com>
2025-08-29 15:48:09 +0800

    use chromium

--- app/lib/puppeteer.ts
+++ app/lib/puppeteer.ts
@@ -1,9 +1,20 @@
+// /lib/puppeteer.ts
 import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 
 export async function launchBrowser() {
+
+
+    // return await puppeteer.launch({
+    //     headless: true,
+    //     executablePath:
+    //         "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", // sesuaikan path di lokal
+    //     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    // });
+
     return await puppeteer.launch({
-        headless: true,
-        executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", // sesuaikan
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        args: chromium.args,
+        executablePath: await chromium.executablePath(),
+        headless: true, // gunakan mode headless baru
     });
 }
