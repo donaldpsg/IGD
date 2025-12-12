2e1ab31001935786b350dc457d9df36c9ec50f40
unknown <donald@peaksolutionsid.com>
2025-12-12 15:29:00 +0800

    add console log

--- app/pdam/page.tsx
+++ app/pdam/page.tsx
@@ -185,6 +185,7 @@ export default function Page() {
 
             const dataAI = await responseAI.json();
             const dataJSON: DataGangguan = JSON.parse(dataAI.text);
+            console.log(dataAI)
 
             if (dataJSON.tanggal === dataJSON.estimasi_pengerjaan) {
                 dataJSON.estimasi_pengerjaan = ""
